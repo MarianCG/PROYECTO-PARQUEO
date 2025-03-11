9c93a2e4f90fc0191477c2c2c73ad532f9c037d8
Portox04 <dibtecportocarrero@gmail.com>
2025-03-10 21:28:43 -0600

    Arreglo de branch

---
+++ proyectoSpaceAdmin/src/proyectospaceadmin/ProyectoSpaceAdmin.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+//Rama ERICK P
+package proyectospaceadmin;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author porto
+ */
+public class ProyectoSpaceAdmin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+//      **INICIALIZACION DE PANTALLA**
+        Park_s1 parqueoS1 = new Park_s1();
+        parqueoS1.setVisible(true);
+    }
+
+}
