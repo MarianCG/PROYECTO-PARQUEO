9ebc336ef6e89f94602e57034ec0dd2aafa5a9c3
angelicaschlubeck <pschlubeck70795@ufide.ac.cr>
2025-04-02 20:14:06 -0600

    Update ProyectoMainNBeans.java

--- ProyectoMainNBeans/src/proyectomainnbeans/ProyectoMainNBeans.java
+++ ProyectoMainNBeans/src/proyectomainnbeans/ProyectoMainNBeans.java
@@ -16,7 +16,7 @@ public class ProyectoMainNBeans {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      //hola Angelica
+      //hola Josafath
       
        Parqueo parqueo = new Parqueo();
        SalaReuniones Sala1 = new SalaReuniones("Sala 1\n", 5);
